6d256f92d16cc72b50d93b11082423820d022982
Auraaawr <151865865+Auraaawr@users.noreply.github.com>
2025-05-31 21:14:38 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+    std::cout << "What hafen vella? Why u crying again? I know, vamfyr right? vamfyr will feyt to me. Edward, what are u doing here? I told you to protect Vella, but you did not!!! I will sure u die Edward, I will sure you die!!! from now on, she is mine!!!" << std::endl;
+    
+    std::cout << "I have died everyday waiting for you, darling dont be afraid i have loved u for a thousand yearssssss, i lab u for a tawsan moreeeee" << std::endl;
+
+    return 0;
+}
